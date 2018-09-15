bba8c9148b1325f8e131610f722d0cca8682312c
raitoti <marfiandhiputra@gmail.com>
2018-09-15 20:05:17 +0800

    third commit

--- app/src/main/java/divascion/marfiandhi/footballmatchschedule/Model/Team/TeamResponse.kt
+++ app/src/main/java/divascion/marfiandhi/footballmatchschedule/Model/Team/TeamResponse.kt
@@ -3,6 +3,4 @@ package divascion.marfiandhi.footballmatchschedule.Model.Team
 /**
  * Created by Marfiandhi on 9/15/2018.
  */
-data class TeamResponse(
-        val teams: List<Team>
-)
+data class TeamResponse( val teams: List<Team> )
